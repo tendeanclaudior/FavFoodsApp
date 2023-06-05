6b35262a1e612187b83c8a9c9800d740ea007fc8
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-06-05 23:09:17 +0800

    Initial Commit

---
+++ FavFoodsApp/FavFoodsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFoodsAppApp.swift
+//  FavFoodsApp
+//
+//  Created by Claudio Tendean on 05/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FavFoodsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
